85a850fd921a98f9a9f887673245738177cb3362
Lucas Dias <luka.dias.pereira@gmail.com>
2022-07-14 09:50:49 -0300

    4b-configurando visibilidade de atributos e metodos

--- src/visibilidadeAtributosMetodos/Caneta.java
+++ src/visibilidadeAtributosMetodos/Caneta.java
@@ -6,7 +6,7 @@ public class Caneta {
     public String cor;
     private float ponta;
     protected int carga;
-    protected boolean tampada;
+    private boolean tampada;
 
     public void status(){
         System.out.println("Modelo: "+this.modelo);
@@ -24,11 +24,11 @@ public class Caneta {
         }
     }
 
-    private void tampar(){
+    public void tampar(){
         this.tampada = true;
     }
 
-    private void destampar(){
+    public void destampar(){
         this.tampada = false;
     }
 
--- src/visibilidadeAtributosMetodos/Main.java
+++ src/visibilidadeAtributosMetodos/Main.java
@@ -6,8 +6,11 @@ public class Main {
         Caneta c1 = new Caneta();
         c1.modelo = "BIC";
         c1.cor = "azul";
-        c1.ponta = 0.5;
-
+        //c1.ponta = 0.5f;
+        c1.carga = 80;
+        c1.destampar();
+        c1.status();
+        c1.rabiscar();
 
     }
 }
